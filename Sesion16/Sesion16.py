7a71ef14020825f74c88ab0632ee56e4ce8265e3
Darksw20 <ricardog.navi20@gmail.com>
2019-10-15 10:43:03 -0500

    finich

---
+++ Sesion16/Sesion16.py
@@ -0,0 +1,12 @@
+miCadena = "Bienvenidos al curso de Algoritmos y Programacion  "
+
+print(len(miCadena))
+print(max(miCadena))
+print(min(miCadena))
+print("hola" in miCadena)
+print(miCadena.isalnum())
+print(miCadena.isupper())
+print(miCadena.startswith("bienve"))
+print(miCadena.rfind("grama"))
+print("a=",miCadena.count("a"),"e=",miCadena.count("e"),"i=",miCadena.count("i"),"o=",miCadena.count("o"),"u=",miCadena.count("u"))
+print(miCadena.strip())
